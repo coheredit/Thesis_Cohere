6ec22b9daadf98867457f81cb21f4591bbc6890a
Rosario, Angelica G. <angelicalykarosario@gmail.com>
2025-06-08 17:06:40 +0800

    create placement code for the viewing of reservation on patron page

--- app/Models/Inquiry.php
+++ app/Models/Inquiry.php
@@ -9,7 +9,34 @@ class Inquiry extends Model
     protected $table = 'inquiry';
 
     protected $fillable = [
-        'patron_id', 'event_type', 'theme_motif', 'venue', 'other_venue',
-        'other_event_type', 'other_theme_motif', 'message', 'date', 'time', 'status'
+        'patron_id',
+        'tracking_code',
+        'event_type',
+        'other_event_type',
+        'theme_motif',
+        'other_theme_motif',
+        'venue',
+        'other_venue',
+        'event_date',
+        'event_time',
+        'message',
+        'status',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($inquiry) {
+            // Only set tracking_code if it's not already provided
+            if (empty($inquiry->tracking_code)) {
+                $inquiry->tracking_code = 'VS-' . strtoupper(substr(uniqid(), -6));
+            }
+        });
+    }
+
+    public function patron()
+    {
+        return $this->belongsTo(Patron::class, 'patron_id');
+    }
 }
