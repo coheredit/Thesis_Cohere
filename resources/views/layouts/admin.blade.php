85b65820feb5a75ef67f5b3ef68cf2f4f6dc8e52
Angelica G. Rosario <angelicalykarosario@gmail.com>
2025-06-11 00:35:46 +0800

    add favicon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Admin Panel')</title>
+
+        <link rel="icon" type="image/png" href="{{ asset('images/vs_logo.png') }}" />
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
 </head>
